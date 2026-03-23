119064adb1eb28ab449733846fac5a53d2d7c2b1
BeenaSazta <veenasazta92@gmail.com>
2026-03-23 10:24:02 +0400

    add global scope for tenant

---
+++ app/Models/Traits/BelongsToFirm.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Models\Traits;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
+trait BelongsToFirm
+{
+    protected static function bootBelongsToFirm()
+    {
+        static::addGlobalScope('firm', function (Builder $builder) {
+            if (Auth::check() && !Auth::user()->isPlatformAdmin()) {
+                $builder->where('firm_id', Auth::user()->firm_id);
+            }
+        });
+
+        static::creating(function ($model) {
+            if (Auth::check() && !Auth::user()->isPlatformAdmin()) {
+                $model->firm_id = Auth::user()->firm_id;
+            }
+        });
+    }
+}
+?>
